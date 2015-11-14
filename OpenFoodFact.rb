ba729162021a0419eaf5982b2bbf5f0f38010bc7
svenisis1 <kazumasis2001@gmail.com>
2015-11-14 11:54:00 +0000

    test OpenFood ruby

---
+++ OpenFoodFact.rb
@@ -0,0 +1,17 @@
+require"open-url"
+require"json"
+
+loop do 
+    puts "Entrez unnuméros de code barre:"
+    bar_code = gets.chomp
+
+    url = "http://fr.openfoodfacts.org/api/v0/produit/#{bar_code}.json"
+
+    json = open(url).read
+
+    data= JSON.parse(json)
+
+    puts data ["product"]["product_name"]
+
+end
+ 
